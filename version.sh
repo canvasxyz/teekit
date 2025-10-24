85c9ad7ce5718acd916e0050625d088709000b60
Raymond Z <raykyri@gmail.com>
2025-10-24 15:35:48 -0400

    chore: add version.sh

---
+++ version.sh
@@ -0,0 +1,36 @@
+if [[ $1 == v* ]]; then
+  echo "invalid version number" && exit 1
+fi
+
+VERSION="\"$1\""
+SET_VERSION=".version |= ${VERSION}"
+
+
+echo "$( jq "${SET_VERSION}" package.json )" > package.json
+
+PACKAGES="$(ls packages/)"
+
+for f in ./package.json; do
+  p=$( cat $f | jq "${SET_VERSION}" )
+  for PACKAGE in ${PACKAGES}; do
+    P="\"@teekit/${PACKAGE}\"";
+    SET_DEPENDENCIES="if .dependencies.${P}? then .dependencies.${P} = ${VERSION} else . end";
+    SET_DEV_DEPENDENCIES="if .devDependencies.${P}? then .devDependencies.${P} = ${VERSION} else . end";
+    p=$( echo "$p" | jq "${SET_DEPENDENCIES} | ${SET_DEV_DEPENDENCIES}" );
+  done;
+  echo "$p" | jq --tab > $f;
+done
+
+for f in ./packages/*/package.json; do
+  p=$( cat $f | jq "${SET_VERSION}" )
+  for PACKAGE in ${PACKAGES}; do
+    P="\"@teekit/${PACKAGE}\"";
+    SET_DEPENDENCIES="if .dependencies.${P}? then .dependencies.${P} = ${VERSION} else . end";
+    SET_DEV_DEPENDENCIES="if .devDependencies.${P}? then .devDependencies.${P} = ${VERSION} else . end";
+    p=$( echo "$p" | jq "${SET_DEPENDENCIES} | ${SET_DEV_DEPENDENCIES}" );
+  done;
+  echo "$p" | jq --tab > $f;
+done
+
+rm package-lock.json
+npm i
