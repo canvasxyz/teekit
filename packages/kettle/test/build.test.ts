54d02146d508f34a13610ae74075f9ce617cc6a0
Raymond Z <raykyri@gmail.com>
2025-11-01 22:39:49 -0400

    chore: add test for worker build

---
+++ packages/kettle/test/build.test.ts
@@ -0,0 +1,22 @@
+import test from "ava"
+import { mkdtempSync, existsSync } from "fs"
+import { tmpdir } from "os"
+import { join } from "path"
+import { fileURLToPath } from "url"
+
+import { buildKettleApp, buildKettleExternals } from "../server/index.js"
+
+test.serial("build worker", async (t) => {
+  const subpackageDir = fileURLToPath(new URL("..", import.meta.url))
+  const targetDir = mkdtempSync(join(tmpdir(), "kettle-build-test"))
+
+  await buildKettleApp({
+    source: join(subpackageDir, "app.ts"),
+    targetDir,
+  })
+  await buildKettleExternals({ targetDir })
+
+  t.true(existsSync(join(targetDir, "app.js")))
+  t.true(existsSync(join(targetDir, "worker.js")))
+  t.true(existsSync(join(targetDir, "externals.js")))
+})
