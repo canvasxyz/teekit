3398dd004877998aa622a8598f78fb4b14abdd97
Raymond Zhong <raykyri@gmail.com>
2025-11-12 05:09:52 +0000

    fix: install.sh on linux

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 INSTALL_PATH=$(npm config get prefix)/bin/kettle
 
-echo "Installing the local development build of the kettle CLI to /usr/bin/dev."
+echo "Installing the local development build of the kettle CLI to $INSTALL_PATH."
 
 if test -f "$INSTALL_PATH"; then
     read -p "Already installed. Overwrite any past install? [y/N] " -n 1 -r
@@ -10,18 +10,16 @@ if test -f "$INSTALL_PATH"; then
     then
         # Overwrite any past install
         unlink ${INSTALL_PATH}
-        echo "#!/usr/bin/env sh" > ${INSTALL_PATH}
+        echo "#!/usr/bin/env bash" > ${INSTALL_PATH}
     else
-        echo
         echo "Aborting, ok!"
         exit 1
     fi
 else
     # Create a new install
     touch ${INSTALL_PATH}
-    echo "#!/usr/bin/env sh" >> ${INSTALL_PATH}
+    echo "#!/usr/bin/env bash" >> ${INSTALL_PATH}
 fi
-echo "node ${PWD}/packages/kettle/services/lib/cli.js \$@" >> ${INSTALL_PATH}
+echo "node ${PWD}/packages/kettle/services/lib/cli.js \"\$@\"" >> ${INSTALL_PATH}
 chmod +x ${INSTALL_PATH}
-echo
 echo "Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@ INSTALL_PATH=$(npm config get prefix)/bin/kettle
 
 if test -f "$INSTALL_PATH"; then
     rm ${INSTALL_PATH}
-    echo "Uninstalled"
+    echo "Uninstalled $INSTALL_PATH"
 else
-    echo "No executable found"
+    echo "No executable found at $INSTALL_PATH"
 fi
